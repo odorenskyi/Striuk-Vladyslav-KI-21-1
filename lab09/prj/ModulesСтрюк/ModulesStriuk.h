557b938cfcc157a296fb74d49992c7848a8d9db8
Vladislav <vvlad1175@gmail.com>
2022-02-23 18:04:43 +0200

    realized function for task 9.1 in ModulesStriuk

--- lab09/prj/ModulesСтрюк/ModulesStriuk.h
+++ lab09/prj/ModulesСтрюк/ModulesStriuk.h
@@ -1,8 +1,8 @@
 #ifndef MODULESSTRIUK_H_INCLUDED
 #define MODULESSTRIUK_H_INCLUDED
-
 #include <cmath>
 
 double s_calculation(float x, float y, float z);
+void salaryAndTaxesOutput(int workingHours[5]);
 
 #endif // MODULESSTRIUK_H_INCLUDED
--- lab09/prj/ModulesСтрюк/main.cpp
+++ lab09/prj/ModulesСтрюк/main.cpp
@@ -5,3 +5,32 @@ double s_calculation(float x, float y, float z)
     const double PI = 3.14159;
     return log(x - y) + sqrt((PI * pow(x, 2)) / x + (z / (2 * pow(y, 2))));
 }
+
+void salaryAndTaxesOutput(unsigned char workingHours[5])
+{
+    float fullSalary = 0;
+    cout << "ПН: " << workingHours[0] << endl
+         << "ВТ: " << workingHours[1] << endl
+         << "СР: " << workingHours[2] << endl
+         << "ЧТ: " << workingHours[3] << endl
+         << "ПТ: " << workingHours[4] << endl << endl;
+
+    for (int i = 0; i < 5; i++) {
+        if (workingHours[i] <= 0)   { continue; }
+        else if (workingHours <= 8) { fullSalary += workingHours * 40; }
+        else                        { fullSalary += workingHours * 40 + 300; }
+    }
+    cout << "Нарахована сума зарплатні: "
+         << fullSalary << " грн" << endl;
+
+    cout << "Податок на прибуток фізособи (15%%): "
+         << fullSalary * 0.15 << " грн" << endl
+         << "Податок до пенсійного фонду (2%%): "
+         << fullSalary * 0.02 << " грн" << endl
+         << "Податок до фонду страхування на випадок безробіття (0.6%%): "
+         << fullSalary * 0.006 << " грн" << endl
+         << "Податок на соціально страхування у випадку втрати працезатності (1%%): "
+         << fullSalary * 0.01 << " грн" << endl;
+}
+
+
