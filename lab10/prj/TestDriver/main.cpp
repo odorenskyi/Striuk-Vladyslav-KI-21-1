2fc66c290e497dbdf8f0f69fd0a07f4e6a448161
Vladislav <vvlad1175@gmail.com>
2022-03-23 11:15:57 +0200

    create TestDriver project | add Write-To-File function for testing

---
+++ lab10/prj/TestDriver/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include "ModulesStriuk.h"
+
+using namespace std;
+
+int main()
+{
+    system("chcp 65001 & cls");
+    fillInputTxtFile("Щастя");
+    return 0;
+}
