cde88912b9138da4c1cac490b6c957eb77d9559a
Vladislav <vvlad1175@gmail.com>
2022-04-11 19:07:52 +0300

    create functions: returning defaullt enroll | auto downloading data

--- lab11/prj/ModulesStriuk/ModulesStriuk.h
+++ lab11/prj/ModulesStriuk/ModulesStriuk.h
@@ -1,4 +1,12 @@
 #ifndef MODULESSTRIUK_H_INCLUDED
 #define MODULESSTRIUK_H_INCLUDED
 
+#include "struct_type_project_1.h"
+
+#include <fstream>
+#include <iostream>
+
+regEnrollment *initilaizeRoot();
+regEnrollment *autoUploadFromFile(regEnrollment *root);
+
 #endif // MODULESSTRIUK_H_INCLUDED
--- lab11/prj/ModulesStriuk/main.cpp
+++ lab11/prj/ModulesStriuk/main.cpp
@@ -1,3 +1,60 @@
 #include "ModulesStriuk.h"
 
 using namespace std;
+
+/// Запис за замовчуванням ///
+regEnrollment *initilaizeRoot()
+{
+    regEnrollment *regEn = new regEnrollment;
+
+    regEn->firstName = "Іван";
+    regEn->lastName = "Іванов";
+    regEn->patronymic = "Іванович";
+    regEn->carBrand = "Toyota";
+    regEn->gradYear = 2012;
+    regEn->dateDay = 12;
+    regEn->dateMonth = 12;
+    regEn->dateYear = 2022;
+    regEn->govNumber = "АА0000АА";
+    regEn->additions = "Немає лобового скла";
+    regEn->ptr = NULL;
+
+    return(regEn);
+}
+
+/// Автоматичне завантаження бази данних з файлу ///
+regEnrollment *autoUploadFromFile(regEnrollment *root)
+{
+    fullRegister.clear();                                               // Очищення вмісту вектора (так надо)
+    regEnrollment *regEn = root;                                        // Локальний вказівник на перший елемент реєстру
+    ifstream inFile("RegisterDataBase.dat", ios::binary | ios::in);
+
+    if (inFile.is_open() == false) {
+        cout << "Файл з даними не існує, або не вдалося знайти" << endl
+             << "Створення нового та запис інформації за замовчуванням..." << endl;
+        inFile.close();
+
+        fullRegister[0] = initilaizeRoot();                             // Ініціалізація першого запису за замовчуванням
+        ofstream outFile("RegisterDataBase.dat", ios::binary | ios::out);
+        outFile.write((char*)&fullRegister, sizeof(fullRegister));      // Запис вектора у файл
+        outFile.close();
+    }
+    else {
+        inFile.read((char*)&fullRegister, sizeof(fullRegister));        // Зчитування вектору з файлу
+        inFile.close();
+        if (fullRegister.empty()) {                                     // Якщо вектор пустий, ініціалізувати перший запис за замовчуванням
+            fullRegister[0] = initilaizeRoot();
+        }
+    }
+    regEn = fullRegister[0];                                            // Присвоєння першого елемента до вказівника
+    if (regEn->ptr != NULL) {                                           // Завершити виконання, якщо це єдиний елемент
+        for (unsigned int i = 1; i < fullRegister.size(); i++) {
+            regEn->ptr = fullRegister[i];                               // Замінити вказівник в поточній структурі на новий структурний ел.
+            regEn = regEn->ptr;                                         // Переприсвоїти локальний вказівник значенням вказівника в ньому
+            if (fullRegister[i]->ptr == NULL) {
+                break;                                                  // Якщо останній елемент - завершити виконання
+            }
+        }
+    }
+    return(root);                                                       // Функція повертає вказівник на перший елемент динамічної структури
+}
