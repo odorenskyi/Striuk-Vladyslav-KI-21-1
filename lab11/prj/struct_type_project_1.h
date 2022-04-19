2ee3fa4d25837b3842218e9a8a1f07a546b49fcb
Vladislav <vvlad1175@gmail.com>
2022-04-19 23:20:00 +0300

    defined constants which may use in write/read functions

--- lab11/prj/struct_type_project_1.h
+++ lab11/prj/struct_type_project_1.h
@@ -1,22 +1,28 @@
 #ifndef STRUCT_TYPE_PROJECT_1_H_INCLUDED
 #define STRUCT_TYPE_PROJECT_1_H_INCLUDED
 
+#define LARGE_TXT_SIZE 255
+#define PROPER_NAME_SIZE 35
+#define GOV_NUMBER_SIZE 8
+#define YEAR_STIRNG_SIZE 4
+#define DATE_STRING_SIZE 2
+
 #include <string>
 
 struct regEnrollment {
-    char *firstName;
-    char *lastName;
-    char *patronymic;
+    char *firstName;    //use PROPER_NAME_SIZE
+    char *lastName;     //use PROPER_NAME_SIZE
+    char *patronymic;   //use PROPER_NAME_SIZE
 
-    char *carBrand;
+    char *carBrand;     //use PROPER_NAME_SIZE
 
-    char *gradYear;
-    char *dateDay;
-    char *dateMonth;
-    char *dateYear;
+    char *gradYear;     //use YEAR_STIRNG_SIZE
+    char *dateDay;      //use DATE_STRING_SIZE
+    char *dateMonth;    //use DATE_STRING_SIZE
+    char *dateYear;     //use YEAR_STIRNG_SIZE
 
-    char *govNumber;
-    char *additions;
+    char *govNumber;    //use GOV_NUMBER_SIZE
+    char *additions;    //use LARGE_TXT_SIZE
 
     regEnrollment *ptr;
 };
