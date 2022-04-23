b1cd137aa9c7f3237c69afd74a0d4c330ca0208b
Vladislav <vvlad1175@gmail.com>
2022-04-23 21:34:04 +0300

    code improvement: realize action menu cycle

--- lab11/prj/project_1_Striuk/main.cpp
+++ lab11/prj/project_1_Striuk/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <conio.h>
 
 #include "ModulesStriuk.h"
 //#include "ModulesBaramba.h"
@@ -19,10 +20,35 @@ int main()
 
     if (dataFile.is_open() == false || dataFile.tellg() == 0) {
         cout << "Could not open the data file! Exit program..." << endl;
+        return 0;
     }
 
     regEnrollment *rootNode = new regEnrollment;
     regEnrollment *bufNode = rootNode;
+    readFromFile(dataFile, bufNode);
+    cout << "<<< Reading complete >>>" << endl << endl;
 
+    while (true) {
+
+        cout << "================= ACTION MENU =================" << endl
+             << "1. Show register data in console" << endl
+             << "2. Write register data in text file" << endl
+             << "3. Add new register enrollment" << endl
+             << "4. Delete register enrollment by state number" << endl
+             << "5. Find enroll by state number" << endl
+             << "6. Exit program" << endl
+             << "===============================================" << endl << endl;
+
+        switch(_getch()) {
+            case '1': /*show register in console*/ break;
+            case '2': /*show register in txt*/ break;
+            case '3': /*add node*/ break;
+            case '4': /*delete node*/ break;
+            case '5': searchInDatabase(bufNode); break;
+            case '6': /*upload to file*/ return 0; break;
+            default: break;
+        }
+    }
+    /*upload to file*/
     return 0;
 }
