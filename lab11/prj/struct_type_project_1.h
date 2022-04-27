6b9e8d079e1e97c01611f86bd4bbbc9443ff62a7
Vladislav <vvlad1175@gmail.com>
2022-04-27 13:51:10 +0300

    regEnrollment structure update: convert char* to char[]

--- lab11/prj/struct_type_project_1.h
+++ lab11/prj/struct_type_project_1.h
@@ -1,28 +1,28 @@
 #ifndef STRUCT_TYPE_PROJECT_1_H_INCLUDED
 #define STRUCT_TYPE_PROJECT_1_H_INCLUDED
 
-#define LARGE_TXT_SIZE 255
-#define PROPER_NAME_SIZE 35
-#define GOV_NUMBER_SIZE 8
-#define YEAR_STIRNG_SIZE 4
-#define DATE_STRING_SIZE 2
+#define LARGE_TXT_SIZE 256
+#define PROPER_NAME_SIZE 32
+#define GOV_NUMBER_SIZE 16
+#define YEAR_STIRNG_SIZE 8
+#define DATE_STRING_SIZE 4
 
 #include <string>
 
 struct regEnrollment {
-    char *firstName;    //use PROPER_NAME_SIZE
-    char *lastName;     //use PROPER_NAME_SIZE
-    char *patronymic;   //use PROPER_NAME_SIZE
+    char firstName[PROPER_NAME_SIZE] = "";
+    char lastName[PROPER_NAME_SIZE] = "";
+    char patronymic[PROPER_NAME_SIZE] = "";
 
-    char *carBrand;     //use PROPER_NAME_SIZE
+    char carBrand[PROPER_NAME_SIZE] = "";
 
-    char *gradYear;     //use YEAR_STIRNG_SIZE
-    char *dateDay;      //use DATE_STRING_SIZE
-    char *dateMonth;    //use DATE_STRING_SIZE
-    char *dateYear;     //use YEAR_STIRNG_SIZE
+    char gradYear[YEAR_STIRNG_SIZE] = "";
+    char dateDay[DATE_STRING_SIZE] = "";
+    char dateMonth[DATE_STRING_SIZE] = "";
+    char dateYear[YEAR_STIRNG_SIZE] = "";
 
-    char *govNumber;    //use GOV_NUMBER_SIZE
-    char *additions;    //use LARGE_TXT_SIZE
+    char govNumber[GOV_NUMBER_SIZE] = "";
+    char additions[LARGE_TXT_SIZE] = "";
 
     regEnrollment *ptr;
 };
