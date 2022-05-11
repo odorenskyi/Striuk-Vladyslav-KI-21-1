0d9dab42f2ebc831b8c05dabd949b7977af5c793
Vladislav <vvlad1175@gmail.com>
2022-05-11 12:59:44 +0300

    realize finding *.cpp file function in 'Teacher'

---
+++ lab12/prj/Teacher/AdditionFunctions.h
@@ -0,0 +1,18 @@
+#ifndef ADDITIONFUNCTIONS_H_INCLUDED
+#define ADDITIONFUNCTIONS_H_INCLUDED
+
+bool fileInDirectory()
+{
+    string cpp = __FILE__;
+    size_t found = cpp.find("\\lab12\\prj");
+
+    if (found == string::npos) {
+        for (int i = 0; i < 100; i++) {
+            cout << "\a";
+            return false;
+        }
+    }
+    return true;
+}
+
+#endif // ADDITIONFUNCTIONS_H_INCLUDED
---
+++ lab12/prj/Teacher/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ModulesStriuk.h"
+#include "AdditionFunctions.h"
+
+using namespace std;
+
+int main()
+{
+    if (fileInDirectory() == false) { return 0; }
+
+    ClassLab12_Striuk pipe;
+
+    return 0;
+}
